d7086e6f31367f992bfb967d3d71a78092969266
Derek Yang <dyang@hamilton.edu>
2023-03-13 00:33:41 -0500

    project started

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,26 @@
-import './ExploreContainer.css';
+import { useEffect, useState } from "react";
+import "./ExploreContainer.css";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+	let [response, setResponse] = useState("");
+	useEffect(() => {
+		fetch("http://localhost:3000/")
+			.then((response) => response.json())
+			.then((data) => setResponse(data.msg));
+	});
+	return (
+		<div className="container">
+			<strong>{response}</strong>
+			<p>
+				Start with Ionic{" "}
+				<a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">
+					UI Components
+				</a>
+			</p>
+		</div>
+	);
 };
 
 export default ExploreContainer;
