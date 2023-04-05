29d538c92c6c782aef0243141e7ac202f5c334e5
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-04-05 18:25:50 -0400

    add ios and android folders. start readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Welcome to the Hamilton Events mobile app frontend!
+
+1. Install dependencies
+
+`npm install`
+
+ionic cap open ios
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Blank</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">HELLOWRDK</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <ExploreContainer />
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;
