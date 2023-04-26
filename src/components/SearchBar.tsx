9e703219224f806870738594214a5734774bebf6
yifanrhymezhou <yifanzhou2020@gmail.com>
2023-04-25 20:58:15 -0400

    search create

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, Component, ReactNode } from 'react';
 import { IonContent } from '@ionic/react';
-import { IonCard, IonCardSubtitle, IonButton, IonButtons, IonToolbar, IonInfiniteScrollContent, IonInfiniteScroll, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
+import { IonCard, IonSearchbar, IonCardSubtitle, IonButton, IonButtons, IonToolbar, IonInfiniteScrollContent, IonInfiniteScroll, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
 import "./SearchBar.css";
 function SearchBar(){
 
