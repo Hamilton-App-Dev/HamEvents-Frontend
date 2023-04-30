517bd2068014c14c4ba56bba91e7c9ec9a81b214
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-04-30 15:29:27 -0400

    fix: fix warnings

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./ExploreContainer.css";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
@@ -6,7 +6,7 @@ import LogoutButton from "./LogoutButton";
 interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
-    let [response, setResponse] = useState("");
+    // let [response, setResponse] = useState("");
     // useEffect(() => {
     //     fetch("http://localhost:3000/")
     //         .then((response) => response.json())
@@ -14,7 +14,7 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
     // });
     return (
         <div className="container">
-            <strong>Response: {response}</strong>
+            {/* <strong>Response: {response}</strong> */}
             <LoginButton />
             <LogoutButton />
         </div>
