17583252a6c382acd320b238c7950a5566d1cfba
Saturniidae L <saturniidael@saturniidaesmbp.hamilton.edu>
2023-08-24 18:56:23 -0400

    minor syntax error fix

--- src/utils/detectDarkMode.tsx
+++ src/utils/detectDarkMode.tsx
@@ -1,9 +1,9 @@
 const detectDarkMode = ():boolean => {
     if (window.matchMedia) {
-        const query = window.matchMedia('prefers-color-scheme: dark');
-        return !query || !!query?.matches; // true or false
+        const query = window.matchMedia("(prefers-color-scheme: dark)");
+        return query.matches;
     }
-    return false;
+    return true;
 }
 
 export default detectDarkMode;
