c2990c0cd991b9891bf3c77f21c38a5ebb217f5f
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-09-27 17:08:23 -0400

    delete android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'HamLaundry-Frontend',
-  webDir: 'build',
-  bundledWebRuntime: false
+    appId: "com.hamiltonappdev.hamevents",
+    appName: "Events Now",
+    webDir: "build",
+    bundledWebRuntime: false,
 };
 
 export default config;
