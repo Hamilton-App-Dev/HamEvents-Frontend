a04979671665a1e3990e1cfd3df5002de73c335e
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-10-02 20:17:45 -0400

    modify colors and more

--- src/components/HappeningNow.tsx
+++ src/components/HappeningNow.tsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { IonBadge, IonItem, IonLabel, IonList } from '@ionic/react';
+import { IonBadge } from "@ionic/react";
 interface EventCardProps {
     startTime: Date;
     endTime: Date;
 }
 
-const EventCard: React.FC<EventCardProps> = 
-    ({startTime, endTime}) => {
-        const currentTime = new Date();
-        const startTimeDate = new Date(startTime);
-        const endTimeDate = new Date(endTime);
-        const isHappening = currentTime >= startTimeDate && currentTime <= endTimeDate;
-        return (
-            <div className="happening">
-                {isHappening && (
-                    <IonBadge color="warning">Happening NOW</IonBadge>
-                )}
-            </div>
-        )
-
-}
+const EventCard: React.FC<EventCardProps> = ({ startTime, endTime }) => {
+    const currentTime = new Date();
+    const startTimeDate = new Date(startTime);
+    const endTimeDate = new Date(endTime);
+    const isHappening =
+        currentTime >= startTimeDate && currentTime <= endTimeDate;
+    //use the color "primary" for happening today.
+    return (
+        <div className="happening">
+            {isHappening && (
+                <IonBadge color="secondary">Happening Now</IonBadge>
+            )}
+        </div>
+    );
+};
 export default EventCard;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,153 +1,154 @@
 //filter and render events
 //need a go back to top button
 import {
-	IonRefresher,
-	IonRefresherContent,
-	RefresherEventDetail,
+    IonRefresher,
+    IonRefresherContent,
+    RefresherEventDetail,
 } from "@ionic/react";
 
 import { Capacitor } from "@capacitor/core";
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, useHistory } from "react-router-dom";
 import {
-	BrowserRouter as Router,
-	useHistory,
-} from "react-router-dom";
-import {
-	IonCard,
-	IonContent,
-	IonSearchbar,
-	IonCardSubtitle,
-	IonInfiniteScrollContent,
-	IonInfiniteScroll,
-	IonCardContent,
-	IonCardHeader,
-	IonCardTitle,
+    IonCard,
+    IonContent,
+    IonSearchbar,
+    IonCardSubtitle,
+    IonInfiniteScrollContent,
+    IonInfiniteScroll,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
 } from "@ionic/react";
 import ScrollToTop from "react-scroll-to-top";
 import { IonIcon } from "@ionic/react";
 import { fastFood, logoIonic } from "ionicons/icons";
-import EventCard from './HappeningNow';
+import EventCard from "./HappeningNow";
 import "./SearchBar.css";
 import transformTime from "./TransformTime";
 type Card = {
-	id: string;
-	name: string;
-	description: string;
-	organization: string;
-	cover_img: string;
-	event_time_start: Date;
-	event_time_end: Date;
-	location: string;
-	food: boolean;
-	cancelled: boolean;
+    id: string;
+    name: string;
+    description: string;
+    organization: string;
+    cover_img: string;
+    event_time_start: Date;
+    event_time_end: Date;
+    location: string;
+    food: boolean;
+    cancelled: boolean;
 };
 
 type Props = {
-	myArrayOfCards: Card[];
-	fetchData: () => void;
+    myArrayOfCards: Card[];
+    fetchData: () => void;
 };
 
 const FilteredCardList: React.FC<Props> = ({ myArrayOfCards, fetchData }) => {
-	const [searchTerm, setSearchTerm] = useState("");
-	const [cards, setCards] = useState(myArrayOfCards.slice(0, 5));
-	const isWeb = Capacitor.getPlatform() === "web";
+    const [searchTerm, setSearchTerm] = useState("");
+    const [cards, setCards] = useState(myArrayOfCards.slice(0, 5));
+    const isWeb = Capacitor.getPlatform() === "web";
 
-	function handleRefresh(event: CustomEvent<RefresherEventDetail>) {
-		fetchData();
-		event.detail.complete();
-	}
+    function handleRefresh(event: CustomEvent<RefresherEventDetail>) {
+        fetchData();
+        event.detail.complete();
+    }
 
-	function loadMoreCards() {
-		console.log("loadMoreCards");
-		const filteredCards = myArrayOfCards.filter((card) =>
-			card.name.toLowerCase().includes(searchTerm.toLowerCase())
-		);
-		const nextCards = filteredCards.slice(cards.length, cards.length + 5);
-		setCards([...cards, ...nextCards]);
-	}
+    function loadMoreCards() {
+        console.log("loadMoreCards");
+        const filteredCards = myArrayOfCards.filter((card) =>
+            card.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        const nextCards = filteredCards.slice(cards.length, cards.length + 5);
+        setCards([...cards, ...nextCards]);
+    }
 
-	const history = useHistory();
-	const handleDetails = (id: string) => {
-		history.push(`/details/${id}`, { data: myArrayOfCards });
-		window.location.href = `/details/${id}`;
-	};
+    const history = useHistory();
+    const handleDetails = (id: string) => {
+        history.push(`/details/${id}`, { data: myArrayOfCards });
+        window.location.href = `/details/${id}`;
+    };
 
-	useEffect(() => {
-		const filteredCards = myArrayOfCards.filter((card) =>
-			card.name.toLowerCase().includes(searchTerm.toLowerCase())
-		);
-		setCards(filteredCards.slice(0, 5));
-	}, [searchTerm, myArrayOfCards]);
+    useEffect(() => {
+        const filteredCards = myArrayOfCards.filter((card) =>
+            card.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setCards(filteredCards.slice(0, 5));
+    }, [searchTerm, myArrayOfCards]);
 
-	return (
-		<IonContent className="container">
-			{!isWeb && (
-				<IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
-					<IonRefresherContent />
-				</IonRefresher>
-			)}
+    return (
+        <IonContent className="container">
+            {!isWeb && (
+                <IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
+                    <IonRefresherContent />
+                </IonRefresher>
+            )}
 
-			<ScrollToTop smooth color="#6f00ff" />
-			<IonSearchbar
-				placeholder="Search for an event..."
-				value={searchTerm}
-				onIonChange={(e) => setSearchTerm(e.detail.value!)}
-			></IonSearchbar>
-			{cards.map((card) => (
-				<IonCard key={card.id}>
-					<IonCardHeader className="header">
-						<div
-							style={{
-								display: "flex",
-								justifyContent: "space-between",
-								alignItems: "center",
-							}}
-						>
-							<IonCardTitle className="cardName">
-								{card.name}
-							</IonCardTitle>
-							<div>
-								{card.food && (
-									<IonIcon
-										icon={fastFood}
-										size="large"
-										color="warning"
-									/>
-								)}
-							</div>
-						</div>
-						<div style={{
-								marginTop: 5,
-								marginBottom: 5
-							}}
-						>
-							<EventCard startTime={card.event_time_start} endTime={card.event_time_end} />
-						</div>
-						<IonCardSubtitle style={{ fontWeight: "bold" }}>
-							{card.organization}
-						</IonCardSubtitle>
-						<IonCardSubtitle>
-							{card.location.split(",")[0]}
-						</IonCardSubtitle>
-						<IonCardSubtitle>
-							{transformTime(card).start} -{" "}
-							{transformTime(card).end}
-						</IonCardSubtitle>
-					</IonCardHeader>
+            <ScrollToTop smooth color="#6f00ff" />
+            <IonSearchbar
+                placeholder="Search for an event..."
+                value={searchTerm}
+                onIonChange={(e) => setSearchTerm(e.detail.value!)}
+            ></IonSearchbar>
+            {cards.map((card) => (
+                <IonCard key={card.id}>
+                    <IonCardHeader className="header">
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                alignItems: "center",
+                            }}
+                        >
+                            <IonCardTitle className="cardName">
+                                {card.name}
+                            </IonCardTitle>
+                            <div>
+                                {card.food && (
+                                    <IonIcon
+                                        icon={fastFood}
+                                        size="large"
+                                        color="primary"
+                                    />
+                                )}
+                            </div>
+                        </div>
+                        <div
+                            style={{
+                                marginTop: 5,
+                                marginBottom: 5,
+                            }}
+                        >
+                            <EventCard
+                                startTime={card.event_time_start}
+                                endTime={card.event_time_end}
+                            />
+                        </div>
+                        <IonCardSubtitle style={{ fontWeight: "bold" }}>
+                            {card.organization}
+                        </IonCardSubtitle>
+                        <IonCardSubtitle>
+                            {card.location.split(",")[0]}
+                        </IonCardSubtitle>
+                        <IonCardSubtitle>
+                            {transformTime(card).start} -{" "}
+                            {transformTime(card).end}
+                        </IonCardSubtitle>
+                    </IonCardHeader>
 
-					<IonCardContent>{card.description}</IonCardContent>
-				</IonCard>
-			))}
-			<IonInfiniteScroll
-				onIonInfinite={(ev) => {
-					loadMoreCards();
-					setTimeout(() => ev.target.complete(), 500);
-				}}
-			>
-				<IonInfiniteScrollContent loadingSpinner="bubbles"></IonInfiniteScrollContent>
-			</IonInfiniteScroll>
-		</IonContent>
-	);
+                    <IonCardContent>{card.description}</IonCardContent>
+                </IonCard>
+            ))}
+            <IonInfiniteScroll
+                onIonInfinite={(ev) => {
+                    loadMoreCards();
+                    setTimeout(() => ev.target.complete(), 500);
+                }}
+            >
+                <IonInfiniteScrollContent loadingSpinner="bubbles"></IonInfiniteScrollContent>
+            </IonInfiniteScroll>
+        </IonContent>
+    );
 };
 
 export default FilteredCardList;
