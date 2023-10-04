fae71a8af261fa1ad877646ab9aaddbfe9994c49
kitan23 <ktran@hamilton.edu>
2023-10-04 12:14:46 -0400

    code no result component and use conditional statement to check if there's any result

---
+++ src/components/NoResult.tsx
@@ -0,0 +1,24 @@
+import { IonContent } from "@ionic/react";
+
+type Props = {
+	searchTerm: string;
+};
+
+const NoResult: React.FC<Props> = ({ searchTerm }) => {
+	return (
+		<div
+			style={{
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				height: "50%",
+				fontSize: "1.2rem",
+				fontWeight: "bold",
+			}}
+		>
+			No results for "{searchTerm}"
+		</div>
+	);
+};
+
+export default NoResult;
